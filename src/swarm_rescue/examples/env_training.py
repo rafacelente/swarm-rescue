bcde88d056f175f44469773f66179d6681ac5674
rafacelente <rafaelmcelente@gmail.com>
2023-11-07 12:16:42 +0100

    ss_73 preparation for training

--- src/swarm_rescue/examples/env_training.py
+++ src/swarm_rescue/examples/env_training.py
@@ -32,16 +32,17 @@ from spg_overlay.utils.misc_data import MiscData
 from maps.map_env import EnvMap
 from random import randrange
 
-#import pyvirtualdisplay
+import pyvirtualdisplay
 
 from stable_baselines3 import PPO
 from stable_baselines3.common.env_util import make_vec_env
+from stable_baselines3.common.callbacks import CheckpointCallback
 from drones.RLDrone import RLDrone
         
 
 def main():
     
-    #pyvirtualdisplay.Display(visible=0, size=(1024, 768)).start()
+    pyvirtualdisplay.Display(visible=0, size=(1024, 768)).start()
     
     my_map = EnvMap()
     my_map.reset_map()
@@ -62,49 +63,56 @@ def main():
 
 
     model = PPO("MlpPolicy", 
-                vec_env, gamma=0.999,
-                n_epochs=32,
+                vec_env, gamma=0.9995,
+                n_epochs=16,
                 gae_lambda=0.95,
-                ent_coef=0.01,
-                batch_size=256,
+                ent_coef=0.001,
+                batch_size=1024,
                 verbose=1,
-                device='cuda',
+                device='cuda',           
                 tensorboard_log=logs_dir
                 )
 
-    
-    
-    
-    model.learn(total_timesteps=2000000, tb_log_name='full_state_first_run', reset_num_timesteps=False)
-    #model = PPO("MlpPolicy", env, verbose=1).learn(5)
+    # Logging and saving
+    checkpoint_callback = CheckpointCallback(
+        save_freq=500000,
+        save_path='./saved_models',
+        name_prefix='ss_73')
 
-    save_path = '/notebooks/saved_models/continuous_04112023.zip'
+    save_path = './saved_models/ss_73_06112023.zip'
+    
+    
+    # Training    
+    model.learn(total_timesteps=4000000, 
+                tb_log_name='full_state_first_run', 
+                reset_num_timesteps=False,
+                callback=checkpoint_callback)
     model.save(save_path)
     
     
-    print('\n\nStarting evaluation\n\n')
-    obs, _ = env.reset()
-    n_steps = 500
-    eval_episodes = 10
-    total_reward = 0
+    # print('\n\nStarting evaluation\n\n')
+    # obs, _ = env.reset()
+    # n_steps = 500
+    # eval_episodes = 10
+    # total_reward = 0
 
-    for episode in range(eval_episodes):
-        obs, _ = env.reset()
-        episode_reward = 0
-        for asd in range(n_steps):
-            action, _ = model.predict(obs, deterministic=True)
-            obs, reward, terminated, truncated, info = env.step(action)
-            done = terminated
-            if asd % 100 == 0 or asd == 600:
-                print(f"obs = {obs}, reward = {reward}, done = {done}")
-            if done:
-                print(f"Goal reached! Reward = {reward}")
-                break
-            episode_reward += reward
-        print(f'Episode {episode}: Reward = {episode_reward}')
-        total_reward += reward
+    # for episode in range(eval_episodes):
+    #     obs, _ = env.reset()
+    #     episode_reward = 0
+    #     for asd in range(n_steps):
+    #         action, _ = model.predict(obs, deterministic=True)
+    #         obs, reward, terminated, truncated, info = env.step(action)
+    #         done = terminated
+    #         if asd % 100 == 0 or asd == 600:
+    #             print(f"obs = {obs}, reward = {reward}, done = {done}")
+    #         if done:
+    #             print(f"Goal reached! Reward = {reward}")
+    #             break
+    #         episode_reward += reward
+    #     print(f'Episode {episode}: Reward = {episode_reward}')
+    #     total_reward += reward
 
-    print(f'Average reward over {eval_episodes} episodes: {total_reward/eval_episodes}')
+    # print(f'Average reward over {eval_episodes} episodes: {total_reward/eval_episodes}')
 
 if __name__ == '__main__':
     main()
